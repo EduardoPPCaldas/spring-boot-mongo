47a218d5f9617ce9f1166f2ee1f47c2720c98440
EduardoPPCaldas <eduardocaldas.dev@gmail.com>
2022-01-27 18:09:04 -0300

    Endpoint para retornar os posts do usuario

--- src/main/java/com/eduardocaldas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eduardocaldas/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.eduardocaldas.workshopmongo.domain.Post;
 import com.eduardocaldas.workshopmongo.domain.User;
 import com.eduardocaldas.workshopmongo.dto.UserDTO;
 import com.eduardocaldas.workshopmongo.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
